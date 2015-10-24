c59b5e206d0f0664f19cb1a5661b195c21a0198f
ANGELAR <angela.rouse12@gmail.com>
2015-10-24 11:39:15 -0400

    Minor Level fixes and Enemy Size

--- Game/code/game.js
+++ Game/code/game.js
@@ -78,7 +78,7 @@ Vector.prototype.times = function(factor) {
 // A Player has a size, speed and position.
 function Player(pos) {
   this.pos = pos.plus(new Vector(0, -0.5));
-  this.size = new Vector(1.8, 1.5);
+  this.size = new Vector(1.4, 1.5);
   this.speed = new Vector(0, 0);
 }
 Player.prototype.type = "player";
